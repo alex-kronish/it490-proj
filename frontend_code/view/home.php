98ee4c54dc0f99051d470024281c8fd19dd53b39
Kevin Solorzano <solorzke@gmail.com>
2019-10-10 00:11:31 -0400

    Fuse both header/footer sections here for the main content

--- frontend_code/view/home.php
+++ frontend_code/view/home.php
@@ -1,16 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>IT490-Project | Home</title>
-</head>
-<body>
+<!-- Header -->
+<?php include 'header.php'; ?>
 
-	<?php 
-		if(isset($_GET['success'])){
-			echo $_GET['success'];
-			echo "'\nRabbitMQ Message published!";
-			echo "'\nEnd of RabbitMQ Testing!";
-		}
-	?>
-</body>
-</html>
+<main role="main" class="container">
+	<div class="starter-template">
+		<h1>Welcome Fellow User!</h1>
+		<p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+	</div>
+
+</main><!-- /.container -->
+
+<?php 
+if(isset($_GET['success'])){
+	echo $_GET['success'];
+	echo "'\nRabbitMQ Message published!";
+	echo "'\nEnd of RabbitMQ Testing!";
+}
+?>
+<!-- Footer -->
+<?php include 'footer.php'; ?>
