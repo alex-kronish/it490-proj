a490be771954fd234acbf174b749977c54f7299e
Kevin Solorzano <solorzke@gmail.com>
2019-10-10 00:10:30 -0400

    Header/Footer page sections are now tangible to use

---
+++ frontend_code/view/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer text-center">
+      <div class="container">
+        <span class="text-muted pull-left">&copy; NJIT | </span><span class="pull-right text-muted">2019-2020</span>
+      </div>
+    </footer>
+  </body>
+</html>
