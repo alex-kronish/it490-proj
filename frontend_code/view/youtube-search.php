8b1bb5c0a406c0b1f385a724533ba16ce549d6bb
Kevin Solorzano <solorzke@gmail.com>
2019-10-12 18:27:39 -0400

    HTML receives search results from controller

--- frontend_code/view/youtube-search.php
+++ frontend_code/view/youtube-search.php
@@ -21,66 +21,8 @@
 	<div class="album py-4 bg-light">
 		<div style="margin-left: 5%; margin-right: 5%; ">
 			<div class="row">
-				<div class="col-md-3">
-					<a href="https://youtube.com/watch?v=meB5aGBONcE">
-					<div class="card mb-4 box-shadow">
-						<img class="card-img-top" alt="Card image cap" src="https://i.ytimg.com/vi/meB5aGBONcE/default_live.jpg" 
-						>
-						<div class="card-body">
-							<p class="card-text">Fortnite The SCIENTIST *NEW* Variant Unlocks! (Fortnite Battle Royale Gameplay)</p>
-							<div class="d-flex justify-content-between align-items-center">
-								<small class="text-muted">9 mins</small>
-							</div>
-						</div>
-					</div>
-				</a>
-				</div>
-				<div class="col-md-3">
-					<div class="card mb-4 box-shadow">
-						<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-						<div class="card-body">
-							<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-							<div class="d-flex justify-content-between align-items-center">
-								<div class="btn-group">
-									<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-									<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-								</div>
-								<small class="text-muted">9 mins</small>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="card mb-4 box-shadow">
-						<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-						<div class="card-body">
-							<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-							<div class="d-flex justify-content-between align-items-center">
-								<div class="btn-group">
-									<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-									<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-								</div>
-								<small class="text-muted">9 mins</small>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-3">
-					<div class="card mb-4 box-shadow">
-						<img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
-						<div class="card-body">
-							<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-							<div class="d-flex justify-content-between align-items-center">
-								<div class="btn-group">
-									<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-									<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-								</div>
-								<small class="text-muted">9 mins</small>
-							</div>
-						</div>
-					</div>
-				</div>
+				<!-- Echo html row from search results here -->
+				<?php $yt_api->echo_html_results($data); ?>
 			</div>
 		</div>
 	</div>
