c3e7df93c951a73407911bbb36af7814cc6cbb79
Kevin Solorzano <solorzke@gmail.com>
2019-10-15 01:59:10 -0400

    Send/Receive files ready for use

--- frontend_code/rabbitmq/receive.php
+++ frontend_code/rabbitmq/receive.php
@@ -30,4 +30,48 @@ function consumeMessage($OPERATION, $CALLBACK)
 	$channel->close();
 	$connection->close();
 }
+
+/* Do not git commit code below until you've tested it properly. */
+function consume($OPERATION, $VHOST, $QUEUE, $CALLBACK)
+{
+	#cred: ip-address, port, username, password, vhost
+	$connection = new AMQPStreamConnection('localhost', 5672, 'kevin', 'kevin', $VHOST);
+	$channel = $connection->channel();
+	$channel->queue_declare($QUEUE, false, false, false, false);
+
+	$parameters = [$OPERATION, $CALLBACK, $channel, $connection];
+
+	echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+	$callback = function ($msg) use ($parameters) {
+		$payload = json_decode($msg->body, true);
+		if($payload['operation'] == $parameters[0])
+			if(is_callable($parameters[1])){
+				call_user_func($parameters[1], $payload, $parameters[2], $parameters[3]);
+			}
+	};
+
+	$channel->basic_consume($QUEUE, '', false, true, false, false, $callback);
+	
+	while ($channel->is_consuming()) {
+			$channel->wait();
+	}
+
+	$channel->close();
+	$connection->close();
+}
+
+/*
+$api = new YouTube_API();
+$a = $api->consume_api_request(function($results){
+	print_r($results);
+});
+
+consume('search-results', 'youtube', 'hello', function($response, $channel, $connection){
+
+	print_r($response);
+	$channel->close();
+	$connection->close();
+}); */
+
 ?>	
--- frontend_code/rabbitmq/send.php
+++ frontend_code/rabbitmq/send.php
@@ -17,4 +17,63 @@ function publishMessage($JSON)
 	$channel->close();
 	$connection->close();
 }
+
+/* Do not git commit code below until you've tested it properly. */
+function produceMessage($JSON, $VHOST, $QUEUE)
+{
+	#cred: ip-address, port, username, password, vhost
+	$connection = new AMQPStreamConnection('localhost', 5672, 'kevin', 'kevin', $VHOST);
+	$channel = $connection->channel();
+	$channel->queue_declare($QUEUE, false, false, false, false);
+	$msg = new AMQPMessage($JSON);
+	$channel->basic_publish($msg, '', $QUEUE);
+	
+	echo " [x] Sent 'Hello World!'\n";
+	$channel->close();
+	$connection->close();
+}
+
+/*
+
+$api = new YouTube_API();
+$api->produce_api_request(array('marvel', 'avengers')); 
+
+
+
+$data = array
+	(
+		'operation' => 'search-results',
+		'video-1' => array
+		(
+			'video-id' => 'LUaj6MrfBsU',
+			'title' => 'The END OF FORTNITE!',
+			'thumbnail' => 'https://i.ytimg.com/vi/LUaj6MrfBsU/default.jpg'
+		),
+
+		'video-2' => array
+		(
+			'video-id' => 'LUaj6MrfBsU',
+			'title' => 'The END OF FORTNITE!',
+			'thumbnail' => 'https://i.ytimg.com/vi/LUaj6MrfBsU/default.jpg'
+		),
+
+		'video-3' => array
+		(
+			'video-id' => 'LUaj6MrfBsU',
+			'title' => 'The END OF FORTNITE!',
+			'thumbnail' => 'https://i.ytimg.com/vi/LUaj6MrfBsU/default.jpg'
+		),
+
+		'video-4' => array
+		(
+			'video-id' => 'LUaj6MrfBsU',
+			'title' => 'The END OF FORTNITE!',
+			'thumbnail' => 'https://i.ytimg.com/vi/LUaj6MrfBsU/default.jpg'
+		)
+	);
+	$data = json_encode($data);
+produceMessage($data, 'youtube', 'hello');
+
+*/
+
 ?>
