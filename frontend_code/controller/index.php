d4a16a2aa1f60caebe6d2e225fc5ac6804c2fb21
Kevin Solorzano <solorzke@gmail.com>
2019-10-15 20:47:52 -0400

    Controller script updated with Session_start()

--- frontend_code/controller/index.php
+++ frontend_code/controller/index.php
@@ -1,8 +1,9 @@
 <?php 
 error_reporting (E_ALL); 
 ini_set ('display_errors', 'on');
-session_start();
 require_once '../model/youtube/youtube-api.php';
+require_once '../model/user.php';
+session_start();
 
 $action = filter_input(INPUT_POST, 'action');
 if(!isset($action))
